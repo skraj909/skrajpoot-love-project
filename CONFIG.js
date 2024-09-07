1ee5de096b8e8174d1cd80e772f9ccc9c76c5ca2
skraj909 <kumarskraj88212@outlook.com>
2024-09-07 17:20:35 +0530

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Hey, there's something I want to say to you. </br> Will you be my love 4ever",
+  descriptionHeader:
+    "Please answer honestly. If you stay silent and leave, that means you AGREE.",
+  buttonYes: "I agree",
+  buttonNo: "I don't agree",
+  titleModar: "Thank you.",
+  descriptionModar:
+    "Thank you for giving me a chance to be by your side, to protect and care for you. </br> I know I'm not handsome, nor very talented, but I have a sincere heart. You are my whole world. </br> I'll pick you up tonight. Dress up nicely! There's a surprise waiting for you.</br> Love you <3",
+};
